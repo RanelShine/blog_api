38e8ebd18aaf2eee03ee97c745905c69ac7e04a0
RanelShine <ranelleshine076@gmail.com>
2025-03-08 19:45:37 +0100

    message

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'posts',
     'users',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,33 +1,42 @@
 """
 URL configuration for blog_api project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from users.views import UserListCreateView
 from posts.views import PostListCreateView, PostDetailView
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_schema_view(title="Blog API")
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="API pour gérer les utilisateurs et les articles de blog",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)  # <-- Parenthèse fermée ici
 
 urlpatterns = [
-    path('users/', UserListCreateView.as_view(), name='user-list'),
-    path('posts/', PostListCreateView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('schema/', schema_view, name='api-schema'),
+    path("users/", UserListCreateView.as_view(), name="user-list"),
+    path("posts/", PostListCreateView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("schema/", schema_view.as_view, name="api-schema"),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
-
-
-
